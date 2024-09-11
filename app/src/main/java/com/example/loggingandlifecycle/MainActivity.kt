721b1cca1ea25851c416f4c97845cb03669987a3
Maya Oniciuc <maya.klmno@gmail.com>
2024-09-11 10:51:51 -0700

    Starter code from lesson

--- app/src/main/java/com/example/loggingandlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/loggingandlifecycle/MainActivity.kt
@@ -1,30 +1,45 @@
 package com.example.loggingandlifecycle
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // in java a static constant was declared how...
+    // public static final int PI = 3
+
+    // in Kotlin without a static keyword we use a companion object
+    companion object {
+        // all of the variables in here will be static
+        val TAG = "MainActivity" // useful for the log functions
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
-            // here is a comment to learn how to use github
-            // hit command+k to commit something (git-> commit in the menu)
-            // select which files you want to include in the commit.
-            // give a description of the commit
-            // select commit and push
+        Log.d(TAG, "onCreate: hi from Oncreate")
 
-            // more changes
 
-            // Hi from the web github
-        }
+        // here is a comment to learn how to use github
+        // hit command+k to commit something (git-> commit in the menu)
+        // select which files you want to include in the commit.
+        // give a description of the commit
+        // select commit and push
+
+        // more changes
+
+        // Hi from the web github
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
 }
+
